92950bbb55e5e7a1da2f20aaadf2ed28f88c0e39
W. Brandon Comfort <wbgc728@gmail.com>
2022-05-25 11:40:23 -0600

    Added friend and reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,12 +4,17 @@ const {
   getThoughtById,
   createThought,
   updateThought,
-  deleteThought
+  deleteThought,
+  createReaction,
+  RemoveReaction
 } = require('../../controllers/thoughtController.js');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
 
-// /api/thoughts/:thoughId
+// /api/thoughts/:thoughtId
 router.route('/:thoughId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/reactionId').post(createReaction).delete(RemoveReaction);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,14 +4,23 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  removeFriend,
+
 } = require('../../controllers/userController.js');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-// /api/user/:userId
+// /api/users/:userId
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/userId
+router.route('/:userId/friends/userId').post(addFriend);
+
+// /api/users/:userId/friends/:userId
+router.route('/:userId/friends/:userId').delete(removeFriend);
 
 
 module.exports = router;
