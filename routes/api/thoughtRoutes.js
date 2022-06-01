6dadf75ed24bfab471ed9f88a9d155f811b8866f
W. Brandon Comfort <wbgc728@gmail.com>
2022-06-01 03:49:41 -0600

    Thought by ID parameter fix

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -13,7 +13,7 @@ const {
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughId').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions/
 router.route('/:thoughtId/reactions').post(createReaction)
